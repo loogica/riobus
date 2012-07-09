0cc7085f675cd1a26eba42b643a3e4cd26411a1b
Felipe cruz <felipecruz@loogica.net>
2012-07-08 21:57:43 -0300

    first code commit

---
+++ app.py
@@ -0,0 +1,33 @@
+import os
+
+from coopy.base import init_persistent_system
+from riobus import RioBus
+
+from flask import Flask, jsonify, render_template
+from collections import OrderedDict
+
+app = Flask(__name__)
+
+riobus = init_persistent_system(RioBus())
+
+@app.route("/")
+def main():
+    return render_template('index.html', lines=riobus.lines)
+    #return jsonify(riobus.lines)
+
+@app.route("/line/html/<line_id>")
+def html_line(line_id):
+    line = riobus.lines[line_id]
+    return render_template('detail.html', line=line)
+
+@app.route("/line/<line_id>")
+def line(line_id):
+    return jsonify(riobus.lines[line_id])
+
+@app.route("/search/<nome>")
+def search(nome):
+    #streets = [street for street in riobus.search(nome)]
+    return jsonify(riobus.search(nome))
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ riobus.py
@@ -0,0 +1,35 @@
+from coopy.decorators import readonly
+
+class RioBus(object):
+    def __init__(self):
+        self.lines = {}
+        self.streets = {}
+
+    def get_or_create_street(self, name, city_name, direction):
+        key = (name, city_name, direction)
+        if key in self.streets:
+            return self.streets[key]
+        else:
+            self.streets[key] = dict(name=name,
+                                     city_name=city_name,
+                                     direction=direction)
+            return self.streets[key]
+
+    def add_street(self, street, line_id):
+        self.lines[line_id]['streets']\
+                           [(street['name'], street['direction'])] = street
+
+    def init_line(self, name):
+        self.lines[name] = dict(name=name,
+                                city=None,
+                                streets=collections.OrderedDict())
+
+    @readonly
+    def search_street(self, nome):
+        from collections import defaultdict
+        result = defaultdict(list)
+        for line_name, line in self.lines.items():
+            for street_name, street in line['streets'].items():
+                if nome.upper() in street['name'].upper():
+                    result[line_name].append(street)
+        return result
